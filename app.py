8fa2e1d55f491991aa6908e024f86bfe7d17cb18
Ripan Debnath <111954951+debnath94@users.noreply.github.com>
2023-06-20 20:00:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,19 @@ from langchain import PromptTemplate
 from langchain.chains import LLMChain
 
 from langchain.memory import ConversationBufferMemory
+
 from langchain.chains import SequentialChain
+
 import streamlit as st
 
 os.environ["OPENAI_API_KEY"]=openai_key
 
 # streamlit framework
 
-st. title("Celebrity Search Results")
+st.title('Celebrity Search Results')
 input_text=st.text_input("Search the topic u want")
 
-# Prompt Templates-1
+# Prompt Templates
 
 first_input_prompt=PromptTemplate(
     input_variables=['name'],
@@ -35,34 +37,31 @@ person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_hist
 dob_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
 descr_memory = ConversationBufferMemory(input_key='dob', memory_key='description_history')
 
-
-# OPENAI LLMS
+## OPENAI LLMS
 llm=OpenAI(temperature=0.8)
 chain=LLMChain(
     llm=llm,prompt=first_input_prompt,verbose=True,output_key='person',memory=person_memory)
 
-# Prompt Templates-2
+# Prompt Templates
 
 second_input_prompt=PromptTemplate(
     input_variables=['person'],
     template="when was {person} born"
 )
 
-
 chain2=LLMChain(
     llm=llm,prompt=second_input_prompt,verbose=True,output_key='dob',memory=dob_memory)
-
-# Prompt Templates-3
+# Prompt Templates
 
 third_input_prompt=PromptTemplate(
     input_variables=['dob'],
     template="Mention 5 major events happened around {dob} in the world"
 )
-
-
 chain3=LLMChain(llm=llm,prompt=third_input_prompt,verbose=True,output_key='description',memory=descr_memory)
 parent_chain=SequentialChain(
     chains=[chain,chain2,chain3],input_variables=['name'],output_variables=['person','dob','description'],verbose=True)
+
+
 
 if input_text:
     st.write(parent_chain({'name':input_text}))
@@ -72,7 +71,6 @@ if input_text:
 
     with st.expander('Major Events'): 
         st.info(descr_memory.buffer)
-
 
 
 
